84b7a355df6cabc94d0ef2796d4bfe91eb30601b
Marcel Kraus <mail@marcelkraus.de>
2015-12-03 17:58:11 +0100

    Installiere und konfiguriere Gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
+
+gulp.task('less', function() {
+    return gulp.src('./src/Resources/less/app.less')
+        .pipe(less())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./web/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/Resources/less/**/*.less', ['less']);
+});
+
+gulp.task('default', ['less', 'watch']);
