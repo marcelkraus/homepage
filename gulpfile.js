f54bbd6a4734112d85cc78452c15cfba20e759a5
Marcel Kraus <mail@marcelkraus.de>
2015-12-07 16:22:40 +0100

    Minimiere Stylesheet für Produktionsumgebung

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
 gulp.task('less', function() {
     return gulp.src('./src/Resources/less/app.less')
         .pipe(less())
+        .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./web/css'));
 });
